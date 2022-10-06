1a119910f4d8dd68a155152c7199f34ff0c45fba
srivishnuc <srivishnu995@gmail.com>
2022-10-06 23:31:26 +0530

    breakpoints scss imported using @use

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App'
 import { BrowserRouter as Router } from 'react-router-dom'
+
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
