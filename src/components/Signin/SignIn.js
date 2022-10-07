3b1c13788429ee3cc1bab83914a274258b2b6d88
srivishnuc <srivishnu995@gmail.com>
2022-10-07 13:19:43 +0530

    Json-server added for mock api , and proxy option removed in webserver

--- src/components/Signin/SignIn.js
+++ src/components/Signin/SignIn.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+const axios = require('axios').default;
 import './SignIn.scss'
 
 const SignIn = () => {
+    useEffect(() => {
+        axios.get('http://localhost:3006/users').then((res) => {
+            console.log(res.data)
+        })
+    }, [])
+
     return (
         <>
             <h1>Sign In Page</h1>
